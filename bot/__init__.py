5c3a210b312ff71c28ae97c807189474f3fcf76d
vpsbotnew <121183724+vpsbotnew@users.noreply.github.com>
2023-03-15 01:37:02 +0630

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -11,13 +11,13 @@ from logging.handlers import RotatingFileHandler
 from .translation import Translation
 
 # Change Accordingly While Deploying To A VPS
-APP_ID = int("12158462")
+APP_ID = int("22270429")
 
-API_HASH = "0b962717d931f4480c46d56c85d409a5"
+API_HASH = "3e2e8f8f0ae56bb0998c264c4ca243cb"
 
-BOT_TOKEN = "6105683190:AAG3RQFXa_LArBNeuyIDuXkL_j-vaKwp0Hk"
+BOT_TOKEN = "5987017187:AAGm6XTEvGilHtD8RwVutuW67S2VDTpr_mI"
 
-DB_URI = "mongodb+srv://pmfilter:pmfilter@cluster0.xvulgll.mongodb.net/?retryWrites=true&w=majority"
+DB_URI = "mongodb+srv://new_user_user1212:thuta12345@cluster0.lhmmymb.mongodb.net/?retryWrites=true&w=majority"
 
 USER_SESSION = "BQBkZBGLgEukQ8PHW22lQRCTKYh0EDhbWbk3TFlQXFSuU0v33mm3Hh-vOK9ejhWBAUHk1-rAezUq9BEGSQKcA8PbyzT98zG8E6rA_uhjmts4MdhkLvqa57Hfj0wAGAKfz920pfA8IOlF-Dbhgx7F0fzgl9jKTPkUKKCQFUMdq2dtoO6e06LUUlDt5MFJaRMjwNcKXhRNmCeIF7KaWkukc6IL36CeV3h9kcipwt5ixujBx_6_CMuVInYYT3OPdXkdSgc9VPdLQjY2fTtfYwYo4BNac9rh7esldq0t5nUygit0eokY7ICkkxtbWbapE8RLufot9tfCrGUdbZmdffmup138UFsxVQA"
 
