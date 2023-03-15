de05f181031b4f3356d6431abca0619ffc0859eb
vpsbotnew <121183724+vpsbotnew@users.noreply.github.com>
2023-03-15 16:16:24 +0630

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -17,7 +17,7 @@ API_HASH = "713c4dfcbc3f6c30ecb7a780eaaf6d3e"
 
 BOT_TOKEN = "6085048303:AAFLr5H4IAlFUitRoFSsaraZOzUnRQgCuIA"
 
-DB_URI = "mongodb+srv://new_user_user1212:thuta12345@cluster0.lhmmymb.mongodb.net/?retryWrites=true&w=majority"
+DB_URI = "mongodb+srv://ynmoviefilter:ynmoviefilter@cluster0.6erlpc6.mongodb.net/?retryWrites=true&w=majority"
 
 USER_SESSION = "BQATjqidhwRn8_7JhVYT9WVrkS5QPRGq8qm658ZkhgxAKz_CqXOCPk24rT_NJ80rBc5SSF2hQeAz-j7E-MEMdJ82Qs5DalCYpU6R9JUrEVX7KYd370N3W2-P64CW2yJvp8HlRl-h7_VUbdYNrUkpCYPyXGL60BPG7Je1etP1kY9x3PeAG4a8U2xccmfeMZ0z5e76ejDdRLbukChZ2c23mV5iKcjD825U7jKGsbbZ7OSSfp1X4A4LzZxzUyghhVegUGkgV8yxaeEzQQlia2FnMsfMte-nYvT3V-ifHi1deGDsfXFRgdeUto0bLzKCkIzhDJWHbhkWiOUVZm5cN7PExW9mAAAAAWIw7noA"
 
